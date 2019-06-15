91796c960a993418557a487b18042f78db884fe0
liangjinwei <liangjinwei@corp.netease.com>
2019-06-15 19:23:08 +0800

    【梁晋玮】接入几种剧情结局

--- Client/Assets/Script/Scene/MainScene.cs
+++ Client/Assets/Script/Scene/MainScene.cs
@@ -6,9 +6,10 @@ public class MainScene:SceneBase
 {
     enum Stage
     {
-        Start,
-        Stage1,
-        End,
+        Start,//女主被出撞死的状态
+        Stage1,//阻止老鼠拨成红灯后，女主被砸死的状态
+        Stage2,//用消防栓拦住汽车，女主成功过马路后，两车相撞的状态
+        GoodEnd,//主角阻止辆车相撞牺牲，结束循环
     }
 
     Stage m_curStage = Stage.Start;
@@ -16,6 +17,9 @@ public class MainScene:SceneBase
     Dictionary<Stage, float> m_dicStageTime = new Dictionary<Stage, float>()
     {
         {Stage.Start,10 },
+        {Stage.Stage1,10 },
+        {Stage.Stage2,10 },
+        {Stage.GoodEnd,100000 },
     };
 
     Transform m_transMainCanvas;
@@ -30,8 +34,19 @@ public class MainScene:SceneBase
     GameObject m_car;
     Vector2 m_carStartOrgPos = new Vector2(-837, -164f);
     Vector2 m_carStartEndPos = new Vector2(393, -164f);
+    Vector2 m_carStage1EndPos = new Vector2(827, -164f);
+    Vector2 m_carStage2EndPos = new Vector2(-173, -164f);
+    Vector2 m_carGoodEndPos = new Vector2(-173, -164f);
     //--------------------------------------------------------
-
+    GameObject m_car2;
+    Vector2 m_car2StartOrgPos = new Vector2(817, -164f);
+    Vector2 m_car2Stage2EndPos = new Vector2(52, -164f);
+    Vector2 m_car2GoodEndPos = new Vector2(304, -164f);
+    //--------------------------------------------------------
+    GameObject m_steel;
+    Vector2 m_steelStartOrgPos = new Vector2(556, 158f);
+    Vector2 m_steelStage1EndPos = new Vector2(556, -115f);
+    //--------------------------------------------------------
     MapGirlNode m_girl;
     //--------------------------------------------------------
     bool m_screenEffectFlag = false;
@@ -51,8 +66,12 @@ public class MainScene:SceneBase
 
         m_car = GameObject.Find("MainCanvas/Main/Panel1/Car1");
         m_car.transform.localPosition = m_carStartOrgPos;
-        //m_carStartOrgPos = m_car.transform.position;
-        //m_carStartEndPos = new Vector2(m_car.transform.position.x + 10, m_car.transform.position.y);
+
+        m_car2 = GameObject.Find("MainCanvas/Main/Panel1/Car2");
+        m_car2.transform.localPosition = m_car2StartOrgPos;
+
+        m_steel = GameObject.Find("MainCanvas/Main/Panel1/Steel");
+        m_steel.transform.localPosition = m_steelStartOrgPos;
 
         m_girl = GameObject.Find("MainCanvas/Main/Panel1/NPC/Girl").GetComponent<MapGirlNode>();
 
@@ -86,6 +105,26 @@ public class MainScene:SceneBase
             m_car.transform.localPosition = m_carStartOrgPos;
             m_girl.Idle();
         }
+
+        if (m_curStage == Stage.Stage1)
+        {
+            m_car.transform.localPosition = m_carStartOrgPos;
+            m_steel.transform.localPosition = m_steelStartOrgPos;
+            m_girl.Idle();
+        }
+
+        if (m_curStage == Stage.Stage2)
+        {
+            m_car.transform.localPosition = m_carStartOrgPos;
+            m_car2.transform.localPosition = m_car2StartOrgPos;
+            m_steel.transform.localPosition = m_steelStartOrgPos;
+            m_girl.Idle();
+        }
+
+        if (m_curStage == Stage.GoodEnd)
+        {
+            //不重置
+        }
     }
 
     void PlayScene()
@@ -94,15 +133,104 @@ public class MainScene:SceneBase
         {
             float distance = m_carStartEndPos.x - m_car.transform.localPosition.x;
 
-            if (distance > 0.1f && timer >5)
+            if (distance > 0.1f)
             {
-                m_car.transform.Translate(new Vector2(1, 0) * 10f * Time.deltaTime);
+                if (timer > 4)
+                {
+                    m_car.transform.Translate(new Vector2(1, 0) * 10f * Time.deltaTime);
+                }   
             }
             else
             {
                 m_girl.Die();
             }
             
+        }
+
+        if (m_curStage == Stage.Stage1)
+        {
+            float carDistance = m_carStage1EndPos.x - m_car.transform.localPosition.x;
+            if (carDistance > 0.1f)
+            {
+                if (timer > 4)
+                {
+                    m_car.transform.Translate(new Vector2(1, 0) * 10f * Time.deltaTime);
+                }
+            }
+
+            float steelDistance = m_steelStage1EndPos.y - m_steel.transform.localPosition.y;
+            if (steelDistance < -0.1f)
+            {
+                if (timer > 8)
+                {
+                    m_steel.transform.Translate(new Vector2(0, -1) * 10f * Time.deltaTime);
+                }
+            }
+            else
+            {
+                m_girl.Die();
+            }
+
+        }
+
+        if (m_curStage == Stage.Stage2)
+        {
+            float carDistance = m_carStage2EndPos.x - m_car.transform.localPosition.x;
+            if (carDistance > 0.1f)
+            {
+                if (timer > 4)
+                {
+                    m_car.transform.Translate(new Vector2(1, 0) * 10f * Time.deltaTime);
+                }
+            }
+
+            float car2Distance = m_car2Stage2EndPos.x - m_car2.transform.localPosition.x;
+            if (car2Distance < -0.1f)
+            {
+                if (timer > 6)
+                {
+                    m_car2.transform.Translate(new Vector2(-1, 0) * 10f * Time.deltaTime);
+                }
+            }
+
+            float steelDistance = m_steelStage1EndPos.y - m_steel.transform.localPosition.y;
+            if (steelDistance < -0.1f)
+            {
+                if (timer > 8)
+                {
+                    m_steel.transform.Translate(new Vector2(0, -1) * 10f * Time.deltaTime);
+                }
+            }
+        }
+
+        if (m_curStage == Stage.GoodEnd)
+        {
+            float carDistance = m_carGoodEndPos.x - m_car.transform.localPosition.x;
+            if (carDistance > 0.1f)
+            {
+                if (timer > 4)
+                {
+                    m_car.transform.Translate(new Vector2(1, 0) * 10f * Time.deltaTime);
+                }
+            }
+
+            float car2Distance = m_car2GoodEndPos.x - m_car2.transform.localPosition.x;
+            if (car2Distance < -0.1f)
+            {
+                if (timer > 6)
+                {
+                    m_car2.transform.Translate(new Vector2(-1, 0) * 10f * Time.deltaTime);
+                }
+            }
+
+            float steelDistance = m_steelStage1EndPos.y - m_steel.transform.localPosition.y;
+            if (steelDistance < -0.1f)
+            {
+                if (timer > 8)
+                {
+                    m_steel.transform.Translate(new Vector2(0, -1) * 10f * Time.deltaTime);
+                }
+            }
         }
     }
 }
